9b46dbf0f4799c3ae4b7cf6101649d8ece8987ff
rafizul896 <rafizulislam896@gmail.com>
2024-12-28 19:09:40 +0600

    add: blog model

---
+++ src/app/modules/blog/blog.model.ts
@@ -0,0 +1,29 @@
+import { model, Schema } from 'mongoose';
+import { IBlog } from './blog.interface';
+
+const blogSchema = new Schema<IBlog>(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Blog = model<IBlog>('Blog', blogSchema);
