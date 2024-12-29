ff26e64e273aa8f922fc94d5bd563f742716bac7
rafizul896 <rafizulislam896@gmail.com>
2024-12-29 20:47:22 +0600

    add: admin delete blog service added

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,4 +1,7 @@
+import AppError from '../../errors/AppError';
+import { Blog } from '../blog/blog.model';
 import { User } from '../user/user.mode';
+import httpStatus from 'http-status-codes';
 
 const blockUserIntoDB = async (id: string) => {
   const result = await User.findByIdAndUpdate(
@@ -12,7 +15,16 @@ const blockUserIntoDB = async (id: string) => {
   return result;
 };
 
-const deleteBlogFromDB = (id) => {};
+const deleteBlogFromDB = async (id: string) => {
+  const blog = await Blog.findById(id);
+
+  if (!blog) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Blog is not Found!');
+  }
+
+  const result = await Blog.findByIdAndDelete(id);
+  return result;
+};
 
 export const AdminServices = {
   blockUserIntoDB,
